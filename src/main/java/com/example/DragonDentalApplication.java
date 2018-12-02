61de3d69580d9f91f03bd3f9487760bb53277e1d
herohung093 <hung.vm2574@gmail.com>
2018-12-02 11:04:13 +1100

    allowed cross origin globally

--- src/main/java/com/example/DragonDentalApplication.java
+++ src/main/java/com/example/DragonDentalApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DragonDentalApplication {
@@ -13,5 +16,14 @@ public class DragonDentalApplication {
     }
     public static void main(String[] args) {
         SpringApplication.run(DragonDentalApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("https://radiant-fjord-77052.herokuapp.com");
+            }
+        };
     }
 }
