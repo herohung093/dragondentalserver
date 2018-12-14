4469d30b21011b5f3bf771858c2fc4ac2f592510
herohung093 <hung.vm2574@gmail.com>
2018-12-14 17:50:37 +1100

    fixed bugs in update order controller and repo

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -78,9 +79,8 @@ public class OrderController {
         return ResponseEntity.ok().body(orderRepo.getAllByDate(start,end));
 
     }
-    @PostMapping("/")
+    @PostMapping(value = "/",consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity createOrder(@RequestBody Order order){
-
         Staff staff = staffRepo.findByName(order.getStaff().getName());
         if( staff == null){
             return ResponseEntity.status(400).body("Staff not found. Bad request");
